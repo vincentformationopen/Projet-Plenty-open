ae76e729ac50c0e17b966cb3835da3c1ab71a6bb
browseo <131001626+vincentformationopen@users.noreply.github.com>
2023-09-08 15:52:33 +0200

    Maj

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-607-1694178757.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"e454ba2a","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"planty.drinks@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Soumission du formulaire","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Veuillez remplir le captcha ci-dessus.","confirmationUrl":""}, '.uagb-block-e454ba2a', 607 );
+});
+ });
